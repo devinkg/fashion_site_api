86c4be58cfb31dbe91a9be4f3c56930bafa10cc0
Devin Godage <devinkanilka@gmail.com>
2022-01-31 23:40:12 +0530

    .env file used for saving MONGO_URL

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 mongoose
     .connect(
-        "mongodb+srv://DeeKay:<password>@cluster0.7hrlb.mongodb.net/shop?retryWrites=true&w=majority"
+        process.env.MONGO_URL
     )
     .then(() => console.log("DB connection Successful!"))
     .catch((err) => {
