e1d0d632c6ed6a056056d17ddc9c9f38e7f1b990
Devin Godage <devinkanilka@gmail.com>
2022-02-02 02:27:29 +0530

    Updated user API added

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,29 @@
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const User = require("../models/User");
+
 const router = require("express").Router();
+
+//Update
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString()
+    }
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id,
+            {
+                $set: req.body //taking everything in that req.body and set it again
+            },
+            { new: true } // by adding this, returning updated values in response, unless it won't
+        );
+
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
