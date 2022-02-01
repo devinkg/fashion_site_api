610e122f47913f592302c350a2ff49b62a8deb4d
Devin Godage <devinkanilka@gmail.com>
2022-02-01 22:06:48 +0530

    Password encryption added

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
-const User = require("../models/User");
 
 const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -8,7 +9,10 @@ router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString(),
     });
 
     try {
@@ -17,7 +21,6 @@ router.post("/register", async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-    
 });
 
 module.exports = router;
