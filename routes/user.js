1270651cb10787b9cde91fcb18d20afce06a6b6e
Devin Godage <devinkanilka@gmail.com>
2022-02-03 00:25:18 +0530

    Delete User API added

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,4 @@
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 const User = require("../models/User");
 
 const router = require("express").Router();
@@ -24,6 +24,18 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
+});
+
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted...")
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
 });
 
 module.exports = router;
