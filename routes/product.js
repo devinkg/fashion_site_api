8108e274c2e2b0e7958c69cfd9dbfad4ecbf6fee
Devin Godage <devinkanilka@gmail.com>
2022-02-06 02:18:18 +0530

    Get Product API added

--- routes/product.js
+++ routes/product.js
@@ -41,12 +41,25 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
 router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
 
     try {
-        await {Product}.findByIdAndDelete(req.params.id)
+        await { Product }.findByIdAndDelete(req.params.id)
         res.status(200).json("Product has been deleted...")
     } catch (err) {
         res.status(500).json(err);
     }
 
 });
+
+//GET PRODUCT
+router.get("/find/:id", async (req, res) => {
+
+    try {
+        const product = await Product.findById(req.params.id);
+        res.status(200).json(product);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+});
+
 
 module.exports = router;
