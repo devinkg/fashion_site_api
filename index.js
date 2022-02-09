addd6f3323351805f4ddd2540dd2a3496a147852
Devin Godage <devinkanilka@gmail.com>
2022-02-10 02:35:17 +0530

    new paths added

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 dotenv.config();
 
@@ -22,6 +24,8 @@ app.use(express.json()); // added to accept JSON values from the requests.
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`Backend server is running!`);
